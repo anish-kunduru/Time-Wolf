fb268daecc5d438862613ab02b5806fefb17786d
John Heinig <jheinig@iastate.edu>
2015-02-18 13:14:54 -0600

    Attempting to add additional project files.

--- main/SuperAwsomeCardGame/src/GameEngine/Card.java
+++ main/SuperAwsomeCardGame/src/GameEngine/Card.java
@@ -2,13 +2,25 @@ package GameEngine;
 
 public class Card {
 
+	//Non mechanical properties
 	private String name;
 	private String description;
 	
+	//Non-action related properties
 	private int costBuy;
 	private int costAttack;
 	private int vp;
+	
+	
+	//Action related properties
+	private int preturnDiscard;
+	private int postturnDiscard;
 	private int drawCards;
+	private int othersDrawCards;
+	private int trashCards;
+	private int trashForPower;
+	private int removeFromPlayArea;
+	private int othersDiscard;
 
 	
 	
