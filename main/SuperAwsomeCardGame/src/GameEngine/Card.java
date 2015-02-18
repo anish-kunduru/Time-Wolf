58b142f409f8f131331402f3a3ae242a9e19d6d1
John Heinig <jheinig@iastate.edu>
2015-02-17 22:46:48 -0600

    These are the basic model items for the card game. Initial version is very light.

---
+++ main/SuperAwsomeCardGame/src/GameEngine/Card.java
@@ -0,0 +1,15 @@
+package GameEngine;
+
+public class Card {
+
+	private String name;
+	private String description;
+	
+	private int costBuy;
+	private int costAttack;
+	private int vp;
+	private int drawCards;
+
+	
+	
+}
