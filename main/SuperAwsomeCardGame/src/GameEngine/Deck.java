5f10d06c264c26dc7757c173b381a027c1e3a60a
John Heinig <jheinig@iastate.edu>
2015-02-21 21:30:48 -0600

    Fleshing out the basic mechanics of the Deck, Hand, and Discard piles.

--- main/SuperAwsomeCardGame/src/GameEngine/Deck.java
+++ main/SuperAwsomeCardGame/src/GameEngine/Deck.java
@@ -1,5 +1,58 @@
 package GameEngine;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Deck {
+	
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	private Random random = new Random();
+	
+	/**
+	 * Adds a card num number of times to the deck.
+	 * @param c adds card c to the deck
+	 * @param number the number of times the card is added.
+	 */
+	public void addCard(Card c, int num) {
+		//We must have a card and a valid number of cards
+		if(c == null) throw new NullPointerException();
+		if(num < 1) throw new IllegalArgumentException();
+		
+		//Add the card to the deck num times.
+		for(int i = 0; i < num; i++) {
+			this.cards.add(c);
+		}
+		
+	}
+	
+	/**
+	 * Draw a card from the deck.
+	 * @return the card drawn
+	 */
+	public Card draw() {
+		
+		if(this.cards.size() == 0) throw new IllegalStateException();
+		
+		//Determine a random card to return
+		int index = this.random.nextInt(this.cards.size() - 1);
+		
+		//Then return and remove it from the deck
+		Card c = this.cards.get(index);
+		this.cards.remove(index);
+		
+		
+		return(c);
+		
+	}
+	
+	/**
+	 * Gets the size of the deck.
+	 * @return the number of cards remaining in the deck
+	 */
+	public int size() {
+		return(this.cards.size());
+	}
+
+
+	
 }
--- main/SuperAwsomeCardGame/src/GameEngine/DiscardPile.java
+++ main/SuperAwsomeCardGame/src/GameEngine/DiscardPile.java
@@ -1,5 +1,18 @@
 package GameEngine;
 
+import java.util.ArrayList;
+
 public class DiscardPile {
+	
+	
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	
+	/**
+	 * Discard a card to the discard pile
+	 * @param c the card to discard
+	 */
+	public void discard(Card c) {
+		cards.add(c);
+	}
 
 }
