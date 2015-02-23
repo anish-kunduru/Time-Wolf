a4cfe420f101f38ee346fe23926784dbe6632d68
John Heinig <jheinig@iastate.edu>
2015-02-22 19:24:56 -0600

    Started the game engine with places to put specific cards.

---
+++ main/SuperAwsomeCardGame/src/GameEngine/GameEngine.java
@@ -0,0 +1,19 @@
+package GameEngine;
+
+public class GameEngine {
+
+	public static void main(String[] args) {
+
+
+		Deck starter = new Deck();  //The deck the player starts with
+		Deck maingame = new Deck(); //The deck for the center of the game.
+
+		//Add cards to the deck here
+		
+		
+		
+		//Rest of the game engine stuff here.
+		
+	}
+
+}
