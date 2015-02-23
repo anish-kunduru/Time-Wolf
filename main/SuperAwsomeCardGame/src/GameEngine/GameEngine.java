d6b2dad7f390a9cbae069a313fa518b7de2a3a7e
Jessica Haynes <jkhaynes@iastate.edu>
2015-02-22 21:35:29 -0600

    Added cards

--- main/SuperAwsomeCardGame/src/GameEngine/GameEngine.java
+++ main/SuperAwsomeCardGame/src/GameEngine/GameEngine.java
@@ -10,6 +10,21 @@ public class GameEngine {
 
 		//Add cards to the deck here
 		
+		//Card paradox causes all other players to go back 10 years by adding a "paradox" card to their deck
+		Card paradox = new Card("Paradox", "There can only be one! All other players go back 10 years", 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				1, true, false, true);
+				
+		//Card scavenge allows the player to draw two additional cards when played from hand
+		Card scavenge = new Card("Scavenge", "Draw two additional cards!", 4, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, true, false, false);
+				
+		//Card bury allows a player to discard two cards and pick up two new ones
+		Card bury = new Card("Bury", "Discard two cards and replace them with two new", 4, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 
+						true, false, true);
+				
+		//Card wormhole makes all other players discard 2 cards from hand
+		Card wormhole = new Card("Wormhole", "All other players must discard 2 cards", 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 
+						true, false, true);
+		
 		
 		
 		//Rest of the game engine stuff here.
