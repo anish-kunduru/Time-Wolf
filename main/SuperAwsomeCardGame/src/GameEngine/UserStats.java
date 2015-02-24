5c8159656ce7c5a556f75a22da9226c11969ce38
Shelbie <sebesmos@iastate.edu>
2015-02-23 19:50:38 -0600

    Stored procedure to get stats info

--- main/SuperAwsomeCardGame/src/GameEngine/UserStats.java
+++ main/SuperAwsomeCardGame/src/GameEngine/UserStats.java
@@ -1,81 +1,110 @@
 package GameEngine;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
- * An object to interface with the database when viewing user statistics
- * This class will help calculate win ratios, karma values, and other helpful statistics based on a users game interaction.
+ * An object to interface with the database when viewing user statistics This
+ * class will help calculate win ratios, karma values, and other helpful
+ * statistics based on a users game interaction.
+ * 
  * @author Shelbie
  *
  */
 public class UserStats {
-	final private int gamesPlayed;
-	final private int gamesWon;
-	final private int totalPoints;
-	final private int karmaScore;
-	final private int userID;
+	private int gamesPlayed;
+	private int gamesWon;
+	private double totalPoints;
+	private int karmaScore;
+	private int userID;
+	private int ID;
 
-	public UserStats(int _userID, int _gamesPlayed, int _gamesWon, int _totalPoints, int _karma)
-	{
-		userID = _userID;
-		gamesPlayed = _gamesPlayed;
-		gamesWon = _gamesWon;
-		totalPoints = _totalPoints;
-		karmaScore = _karma;
+	public UserStats(int userID) {
+		getStats(userID);
 	}
-	
-	
+
 	/**
 	 * Total number of games played
+	 * 
 	 * @return gamesPlayed
 	 */
-	public int getGamesPlayed()
-	{
+	public int getGamesPlayed() {
 		return gamesPlayed;
 	}
-	
+
 	/**
 	 * Returns the win loss ratio
+	 * 
 	 * @return (# of games won) / ( # of games lost)
 	 */
-	public double getWinLossRatio()
-	{
-		return gamesWon/(gamesPlayed-gamesWon);
+	public double getWinLossRatio() {
+		return gamesWon / (gamesPlayed - gamesWon);
 	}
-	
+
 	/**
 	 * Returns total points accumulated over all games
+	 * 
 	 * @return totalPoints
 	 */
-	public int getTotalPoints()
-	{
+	public double getTotalPoints() {
 		return totalPoints;
 	}
-	
+
 	/**
 	 * Returns average number of points over all games
+	 * 
 	 * @return totalPoints/gamesPlayed
 	 */
-	public double getAveragePoints()
-	{
-		return totalPoints/gamesPlayed;
+	public double getAveragePoints() {
+		return totalPoints / gamesPlayed;
 	}
-	
+
 	/**
-	 * TBD once we figure out how to use karma
-	 * Will interface with the Feedback table in the database
+	 * TBD once we figure out how to use karma Will interface with the Feedback
+	 * table in the database
+	 * 
 	 * @return
 	 */
-	public int getKarma()
-	{
+	public int getKarma() {
 		return 0;
 	}
-	
+
 	/**
 	 * Retrieves stats from the database matching the given userID
+	 * 
 	 * @param userID
 	 * @return corresponding user's statistic information
+	 * @throws SQLException
 	 */
-	public UserStats getStats(int userID)
-	{
-		//Developing 2/23 PM
-		
+	public void getStats(int userID) {
+		try {
+			// Developing 2/23 PM
+			Connection conn = DriverManager.getConnection(
+					"mysql.cs.iastate.edu", "u30911", "4rv2ucue78");
+			String query = "SELECT * FROM Statistics WHERE UserID=" + userID;
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+
+			if (rs.first()) // should only be one returned on table is incorrect
+			{
+				this.gamesPlayed = rs.getInt("TotalGames");
+				this.gamesWon = rs.getInt("TotalWins");
+				this.totalPoints = rs.getDouble("TotalPoints");
+				this.ID = rs.getInt("ID");
+				this.userID = userID;
+			} else {
+				// given userID is not valid
+				this.gamesPlayed = 0;
+				this.gamesWon = 0;
+				this.totalPoints = 0;
+				this.ID = 0;
+				this.userID = 0;
+			}
+		} catch (Exception ex) {
+		}
+
 	}
 }
