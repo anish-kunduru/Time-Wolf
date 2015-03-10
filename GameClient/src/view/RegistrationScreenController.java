9e873db1001b29a48faa59c799b7024fdb92d778
Anish Kunduru <akunduru@iastate.edu>
2015-03-10 17:10:59 -0500

    It is very important that you use the FXML components and not the AWT components.

--- GameClient/src/view/RegistrationScreenController.java
+++ GameClient/src/view/RegistrationScreenController.java
@@ -6,18 +6,18 @@
 
 package view;
 
-import java.awt.Button;
-import java.awt.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 
 public class RegistrationScreenController implements ControlledScreen
 {
-   
+
    // So we can set the screen's parent later on.
    MainController parentController;
-   
+
    @FXML
    private Button registerButton;
    @FXML
@@ -28,32 +28,32 @@ public class RegistrationScreenController implements ControlledScreen
    private PasswordField passwordField;
    @FXML
    private PasswordField checkPasswordField;
-   
+
    private String usernameString;
    private String emailString;
    private String passwordString;
-   
+
    @FXML
    public void initialize()
    {
-      
-	   registerButton.setOnAction(event ->
-	      {
-	         //TO DO - Verify PWs match
-	    	 //TO DO - Check to see if username already exists
-	    	  
-	    	  usernameString = usernameTextField.getText();
-	    	  emailString = emailTextField.getText();
-	          passwordString = passwordField.getText();
-	          
-	          // DEBUG
-	          System.out.println(usernameString);
-	          System.out.println(emailString);
-	          System.out.println(passwordString);
-	      });
-	   
+
+      registerButton.setOnAction(event ->
+      {
+         // TO DO - Verify PWs match
+         // TO DO - Check to see if username already exists
+
+         usernameString = usernameTextField.getText();
+         emailString = emailTextField.getText();
+         passwordString = passwordField.getText();
+
+         // DEBUG
+         System.out.println(usernameString);
+         System.out.println(emailString);
+         System.out.println(passwordString);
+      });
+
    }
-   
+
    /**
     * This method will allow for the injection of each screen's parent.
     */
