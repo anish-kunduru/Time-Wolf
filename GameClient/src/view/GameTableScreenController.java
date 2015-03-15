6adfb5a94420efb8602cc4a9f0108037aa23a474
Anish Kunduru <akunduru@iastate.edu>
2015-03-15 17:08:29 -0500

    Added some game table event handlers. Added additional registration fields (unlinked/ logic not implemented).

--- GameClient/src/view/GameTableScreenController.java
+++ GameClient/src/view/GameTableScreenController.java
@@ -8,6 +8,8 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.TextFlow;
@@ -48,12 +50,70 @@ public class GameTableScreenController implements ControlledScreen
    @FXML
    public void initialize()
    {
-      playerHandOne.setImage(new Image("cards/bite.png"));
+      // Initial cards in deck state.
       // cardsInDeckString.setText("Cards in Deck: NUMCARDS");
-
+      
+      // Initial card states.
+      playerHandOne.setImage(new Image("cards/bite.png"));
+      playerHandTwo.setImage(new Image("cards/claw.png"));
+      playerHandThree.setImage(new Image("cards/edison.png"));
+      playerHandFour.setImage(new Image("cards/sacrifice.png"));
+      playerHandFive.setImage(new Image("cards/genghisKhan.png"));
+      
+      // Add effects to cards.
+      highlightOnMouseEntered(playerHandOne);
+      highlightOnMouseEntered(playerHandTwo);
+      highlightOnMouseEntered(playerHandThree);
+      highlightOnMouseEntered(playerHandFour);
+      highlightOnMouseEntered(playerHandFive);
+      
+      unhighlightOnMouseExited(playerHandOne);
+      unhighlightOnMouseExited(playerHandTwo);
+      unhighlightOnMouseExited(playerHandThree);
+      unhighlightOnMouseExited(playerHandFour);
+      unhighlightOnMouseExited(playerHandFive);
+      
+      // Act as if card was played.
       playerHandOne.setOnMousePressed(event ->
       {
          playerHandOne.setImage(new Image("cards/bury.png"));
+      });
+   }
+   
+   /**
+    * Helper method associates a listener to each passed image that adds a highlight effect to an image on mouse hover.
+    * 
+    * @param imageToHighlight The image that you wish to add the highlight effect to.
+    */
+   private void highlightOnMouseEntered(ImageView imageToHighlight)
+   {
+      imageToHighlight.setOnMouseEntered(event ->
+      {
+         // Create ambient light
+         Light.Distant light = new Light.Distant();
+         light.setAzimuth(-135.0);
+
+         // Create lighting effect
+         Lighting lighting = new Lighting();
+         lighting.setLight(light);
+         lighting.setSurfaceScale(4.0);
+
+         // Apply lighting.
+         imageToHighlight.setEffect(lighting);
+      });
+   }
+   
+   /**
+    * Helper method that removes any effect added to an image when the mouse is no longer hovering over it.
+    * Intended to be used with the highlightOnMouseEntered() method.
+    * 
+    * @param imageToUnhighlight The image that you wish to remove effects from.
+    */
+   private void unhighlightOnMouseExited(ImageView imageToUnhighlight)
+   {
+      imageToUnhighlight.setOnMouseExited(event ->
+      {
+         imageToUnhighlight.setEffect(null);
       });
    }
 
