4312d00184390122545150135ee8b0fa7ccd21f6
Anish Kunduru <akunduru@iastate.edu>
2015-03-15 04:22:24 -0500

    Added more game table logic. Created card images.

--- GameClient/src/view/GameTableScreenController.java
+++ GameClient/src/view/GameTableScreenController.java
@@ -5,13 +5,35 @@
 
 package view;
 
-import GameServer.GameEngine.Hand;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
 
 public class GameTableScreenController implements ControlledScreen
 {
-   // Game state data.
-   Hand playerHand;
+   // Game State Data
+   
+   // FXML Components
+   @FXML
+   private Label cardsInDeckString;
+   
+   @FXML
+   private ImageView playerHandOne;
+   @FXML
+   private ImageView playerHandTwo;
+   @FXML
+   private ImageView playerHandThree;
+   @FXML
+   private ImageView playerHandFour;
+   @FXML
+   private ImageView playerHandFive;
+   
+   @FXML
+   private ImageView lastDiscardImage;
+   
+   @FXML
+   private TextFlow playLog;
    
    // So we can set the screen's parent later on.
    MainController parentController;
@@ -23,7 +45,8 @@ public class GameTableScreenController implements ControlledScreen
    @FXML
    public void initialize()
    {
-      
+      //playerHandOne.setImage(Image value);
+      //cardsInDeckString.setText("Cards in Deck: NUMCARDS");
    }
    
    
