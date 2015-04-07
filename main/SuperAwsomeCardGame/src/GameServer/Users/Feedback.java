7dfd63b677bb26c06161d3547df59e2d29103b09
Shelbie <sebesmos@iastate.edu>
2015-04-07 09:53:51 -0500

    Constructors

--- main/SuperAwsomeCardGame/src/GameServer/Users/Feedback.java
+++ main/SuperAwsomeCardGame/src/GameServer/Users/Feedback.java
@@ -1,5 +1,25 @@
 package GameServer.Users;
 
 public class Feedback {
-
+	private int ID;
+	private int userID;
+	private String description;
+	private boolean isPositive;
+	
+	public Feedback()
+	{
+		ID = 0;
+		userID = 0;
+		description = "";
+		isPositive = false;
+	}
+	
+	public Feedback(int id, int userid, String desc, boolean isPositive)
+	{
+		this.ID = id;
+		this.userID = userid;
+		this.description = desc;
+		this.isPositive = isPositive;
+		
+	}
 }
