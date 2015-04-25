59109cf3fadd71ea101963d6d07f1b27e34f7881
Jessica Haynes <jkhaynes@iastate.edu>
2015-04-24 21:29:54 -0500

    Started work on leaderboard page functionality

--- GameClient/src/leaderboards/LeaderboardsScreenController.java
+++ GameClient/src/leaderboards/LeaderboardsScreenController.java
@@ -6,28 +6,94 @@
 
 package leaderboards;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import GameServer.Users.User;
+import profile.KarmaRow;
+import profile.RecentGameStatsRow;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import singleton.MainModel;
+import userListing.UserRow;
 import view.ControlledScreen;
 import view.MainController;
 
-public class LeaderboardsScreenController implements ControlledScreen
-{
-   // So we can set the screen's parent later on.
-   MainController parentController;
+public class LeaderboardsScreenController implements ControlledScreen {
 
-   /**
-    * Initializes the controller class. Automatically called after the FXML file has been loaded.
-    */
-   @FXML
-   public void initialize()
-   {
-   }
+	private ObservableList<LeaderboardRow> tableData;
+	@FXML
+	private TableView<LeaderboardRow> leaderboard;
 
-   /**
-    * This method will allow for the injection of each screen's parent.
-    */
-   public void setScreenParent(MainController screenParent)
-   {
-      parentController = screenParent;
-   }
+	@FXML
+	private TableColumn rankColumn;
+	@FXML
+	private TableColumn usernameColumn;
+	@FXML
+	private TableColumn totalPointsColumn;
+	@FXML
+	private TableColumn avgPointsColumn;
+	@FXML
+	private TableColumn gamesPlayedColumn;
+	@FXML
+	private TableColumn gamesWonColumn;
+	@FXML
+	private TableColumn ratioColumn;
+	@FXML
+	private TableColumn karmaColumn;
+
+	// So we can set the screen's parent later on.
+	MainController parentController;
+
+	/**
+	 * Initializes the controller class. Automatically called after the FXML
+	 * file has been loaded.
+	 */
+	@FXML
+	public void initialize() {
+		rankColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("rank"));
+		usernameColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("username"));
+		totalPointsColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("totalPoints"));
+		avgPointsColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("avgPoints"));
+		gamesPlayedColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("gamesPlayed"));
+		gamesWonColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("gamesWon"));
+		ratioColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("ratio"));
+		karmaColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("karma"));
+
+		tableData = FXCollections.observableArrayList();
+		leaderboard.setItems(tableData);
+
+		ArrayList<User> users = new ArrayList<User>();
+		
+		try {
+			users = MainModel.getModel().currentLoginData().getLogInConnection().getLeaderboard();
+			
+			for (int i = 0; i < users.size(); i++) {
+				User user = users.get(i);
+				LeaderboardRow currentRow = new LeaderboardRow();
+				currentRow.gamesPlayed.set(user.Statistics.getGamesPlayed());
+				currentRow.gamesWon.set(user.Statistics.getGamesWon());
+				currentRow.ratio.set(user.Statistics.getWinLossRatio());
+				currentRow.totalPoints.set(user.Statistics.getTotalPoints());
+				currentRow.avgPoints.set(user.Statistics.getAveragePoints());
+				currentRow.karma.set(user.Statistics.getKarma());
+
+				tableData.add(currentRow);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	/**
+	 * This method will allow for the injection of each screen's parent.
+	 */
+	public void setScreenParent(MainController screenParent) {
+		parentController = screenParent;
+	}
 }
