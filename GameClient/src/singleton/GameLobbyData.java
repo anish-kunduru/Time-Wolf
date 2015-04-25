d65537e20d1854b76519a86460ce5f6c6080e156
Jessica Haynes <jkhaynes@iastate.edu>
2015-04-25 18:24:17 -0500

    functionality for join game complete

--- GameClient/src/singleton/GameLobbyData.java
+++ GameClient/src/singleton/GameLobbyData.java
@@ -11,10 +11,20 @@ public class GameLobbyData
    // Declare elements necessary to search for the game.
    // (These will need to be called in game lobby's initialization and the page will need to be reloaded on a search call.)
 
+	private int id;
    /**
     * Default constructor to use in singleton.
     */
    public GameLobbyData()
    {
    }
+   
+   public int getID(){
+	   return this.id;
+   }
+   
+   public void setID(int id){
+	   this.id = id;
+   }
+   
 }
