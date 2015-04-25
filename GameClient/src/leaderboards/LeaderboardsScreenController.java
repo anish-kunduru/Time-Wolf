fc193ca6dab3f5ca65c3ff962023a2bb5b70679c
Shelbie <sebesmos@iastate.edu>
2015-04-25 18:20:21 -0500

    update to not display stats of banned users

--- GameClient/src/leaderboards/LeaderboardsScreenController.java
+++ GameClient/src/leaderboards/LeaderboardsScreenController.java
@@ -26,87 +26,104 @@ import singleton.MainModel;
 import userListing.UserRow;
 import view.MainController;
 
-public class LeaderboardsScreenController implements ControlledScreen
-{
-   private ObservableList<LeaderboardRow> tableData;
-   @FXML
-   private TableView<LeaderboardRow> leaderboard;
+public class LeaderboardsScreenController implements ControlledScreen {
+	private ObservableList<LeaderboardRow> tableData;
+	@FXML
+	private TableView<LeaderboardRow> leaderboard;
 
-   @FXML
-   private TableColumn rankColumn;
-   @FXML
-   private TableColumn usernameColumn;
-   @FXML
-   private TableColumn totalPointsColumn;
-   @FXML
-   private TableColumn avgPointsColumn;
-   @FXML
-   private TableColumn gamesPlayedColumn;
-   @FXML
-   private TableColumn gamesWonColumn;
-   @FXML
-   private TableColumn ratioColumn;
-   @FXML
-   private TableColumn karmaColumn;
+	@FXML
+	private TableColumn rankColumn;
+	@FXML
+	private TableColumn usernameColumn;
+	@FXML
+	private TableColumn totalPointsColumn;
+	@FXML
+	private TableColumn avgPointsColumn;
+	@FXML
+	private TableColumn gamesPlayedColumn;
+	@FXML
+	private TableColumn gamesWonColumn;
+	@FXML
+	private TableColumn ratioColumn;
+	@FXML
+	private TableColumn karmaColumn;
 
-   // So we can set the screen's parent later on.
-   MainController parentController;
+	// So we can set the screen's parent later on.
+	MainController parentController;
 
-   /**
-    * Initializes the controller class. Automatically called after the FXML file has been loaded.
-    */
-   @FXML
-   public void initialize()
-   {
-      rankColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("rank"));
-      usernameColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("username"));
-      totalPointsColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("totalPoints"));
-      avgPointsColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("avgPoints"));
-      gamesPlayedColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("gamesPlayed"));
-      gamesWonColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("gamesWon"));
-      ratioColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("ratio"));
-      karmaColumn.setCellValueFactory(new PropertyValueFactory<UserRow, String>("karma"));
+	/**
+	 * Initializes the controller class. Automatically called after the FXML
+	 * file has been loaded.
+	 */
+	@FXML
+	public void initialize() {
+		rankColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"rank"));
+		usernameColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"username"));
+		totalPointsColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"totalPoints"));
+		avgPointsColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"avgPoints"));
+		gamesPlayedColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"gamesPlayed"));
+		gamesWonColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"gamesWon"));
+		ratioColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"ratio"));
+		karmaColumn
+				.setCellValueFactory(new PropertyValueFactory<UserRow, String>(
+						"karma"));
 
-      tableData = FXCollections.observableArrayList();
-      leaderboard.setItems(tableData);
+		tableData = FXCollections.observableArrayList();
+		leaderboard.setItems(tableData);
 
-      ArrayList<User> users = new ArrayList<User>();
+		ArrayList<User> users = new ArrayList<User>();
 
-      try
-      {
-         users = MainModel.getModel().currentLoginData().getLogInConnection().getLeaderboard();
-         int count = 1;
+		try {
+			users = MainModel.getModel().currentLoginData()
+					.getLogInConnection().getLeaderboard();
+			int count = 1;
 
-         for (int i = 0; i < users.size(); i++)
-         {
-        	DecimalFormat df = new DecimalFormat("#0.00");
-            User user = users.get(i);
-            LeaderboardRow currentRow = new LeaderboardRow();
-            currentRow.rank.set(count++);
-            currentRow.username.set(user.getUsername());
-            currentRow.gamesPlayed.set(user.Statistics.getGamesPlayed());
-            currentRow.gamesWon.set(user.Statistics.getGamesWon());
-            currentRow.ratio.set(df.format(user.Statistics.getWinLossRatio()));
-            currentRow.totalPoints.set((int)user.Statistics.getTotalPoints());
-            currentRow.avgPoints.set((int) user.Statistics.getAveragePoints());
-            currentRow.karma.set(user.Statistics.getKarma());
+			for (int i = 0; i < users.size(); i++) {
+				DecimalFormat df = new DecimalFormat("#0.00");
+				User user = users.get(i);
+				if (!user.isBanned()) {
+					LeaderboardRow currentRow = new LeaderboardRow();
+					currentRow.rank.set(count++);
+					currentRow.username.set(user.getUsername());
+					currentRow.gamesPlayed
+							.set(user.Statistics.getGamesPlayed());
+					currentRow.gamesWon.set(user.Statistics.getGamesWon());
+					currentRow.ratio.set(df.format(user.Statistics
+							.getWinLossRatio()));
+					currentRow.totalPoints.set((int) user.Statistics
+							.getTotalPoints());
+					currentRow.avgPoints.set((int) user.Statistics
+							.getAveragePoints());
+					currentRow.karma.set(user.Statistics.getKarma());
 
-            tableData.add(currentRow);
-         }
-      }
-      catch (SQLException e)
-      {
-         e.printStackTrace();
-      }
+					tableData.add(currentRow);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-   }
+	}
 
-   /**
-    * This method will allow for the injection of each screen's parent.
-    */
-   @Override
-   public void setScreenParent(AbstractScreenController screenParent)
-   {
-      parentController = (MainController) screenParent;
-   }
+	/**
+	 * This method will allow for the injection of each screen's parent.
+	 */
+	@Override
+	public void setScreenParent(AbstractScreenController screenParent) {
+		parentController = (MainController) screenParent;
+	}
 }
