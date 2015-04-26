491289993f1b7537e50ad311c1ab8349655a83c9
Shelbie <sebesmos@iastate.edu>
2015-04-25 19:24:26 -0500

    dfkjdfjk

--- GameClient/src/createGame/CreateGameScreenController.java
+++ GameClient/src/createGame/CreateGameScreenController.java
@@ -8,6 +8,7 @@ package createGame;
 
 import framework.AbstractScreenController;
 import framework.ControlledScreen;
+import singleton.MainModel;
 import view.MainController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -15,55 +16,66 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class CreateGameScreenController implements ControlledScreen
-{
-   // FXML Components.
-   @FXML
-   private Button createGameButton;
+public class CreateGameScreenController implements ControlledScreen {
+	// FXML Components.
+	@FXML
+	private Button createGameButton;
 
-   @FXML
-   private TextField lobbyNameTextField;
-   @FXML
-   private TextField passwordTextField;
-   @FXML
-   private ComboBox numberPlayersComboBox;
+	@FXML
+	private TextField lobbyNameTextField;
+	@FXML
+	private TextField passwordTextField;
+	@FXML
+	private ComboBox<Integer> numberPlayersComboBox;
 
-   @FXML
-   private CheckBox chatCheckBox;
+	@FXML
+	private CheckBox chatCheckBox;
 
-   // So we can set the screen's parent later on.
-   MainController parentController;
+	// So we can set the screen's parent later on.
+	MainController parentController;
 
-   /**
-    * Initializes the controller class. Automatically called after the FXML file has been loaded.
-    */
-   @FXML
-   public void initialize()
-   {
-      // Set appropriate text if box is unchecked.
-      chatCheckBox.setOnAction(event ->
-      {
-         if (chatCheckBox.isSelected())
-            chatCheckBox.setText("On");
-         else
-            chatCheckBox.setText("Off");
-      });
+	/**
+	 * Initializes the controller class. Automatically called after the FXML
+	 * file has been loaded.
+	 */
+	@FXML
+	public void initialize() {
+		numberPlayersComboBox.getItems().add(2);
+		numberPlayersComboBox.getItems().add(3);
+		numberPlayersComboBox.getItems().add(4);
+		numberPlayersComboBox.getSelectionModel().select(0);
 
-      createGameButton.setOnAction(event ->
-      {
-         // TO-DO: CHECK IF EVERYTHING WAS SELECTED PROPERLY. (VERIFY INPUT)
-         // TO-DO: CREATE THE GAME.
+		// Set appropriate text if box is unchecked.
+		chatCheckBox.setOnAction(event -> {
+			if (chatCheckBox.isSelected())
+				chatCheckBox.setText("On");
+			else
+				chatCheckBox.setText("Off");
+		});
 
-         parentController.goToGameTableScreen();
-      });
-   }
+		createGameButton.setOnAction(event -> {
+			int players = numberPlayersComboBox.getSelectionModel()
+					.getSelectedItem();
+			String name = lobbyNameTextField.getText();
 
-   /**
-    * This method will allow for the injection of each screen's parent.
-    */
-   @Override
-   public void setScreenParent(AbstractScreenController screenParent)
-   {
-      parentController = (MainController) screenParent;
-   }
+			int id = 0;
+			try {
+				id = MainModel.getModel().gameLobbyData().getGameManager()
+						.createGame(players, name);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			MainModel.getModel().gameLobbyData().setID(id);
+			parentController.goToGameTableScreen();
+		});
+	}
+
+	/**
+	 * This method will allow for the injection of each screen's parent.
+	 */
+	@Override
+	public void setScreenParent(AbstractScreenController screenParent) {
+		parentController = (MainController) screenParent;
+	}
 }
