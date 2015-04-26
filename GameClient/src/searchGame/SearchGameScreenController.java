9dcf6787bbb68de392fa815d020d0e14d4e529c5
Shelbie <sebesmos@iastate.edu>
2015-04-25 19:42:33 -0500

    minor

--- GameClient/src/searchGame/SearchGameScreenController.java
+++ GameClient/src/searchGame/SearchGameScreenController.java
@@ -15,58 +15,54 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class SearchGameScreenController implements ControlledScreen
-{
-   // FXML Components.
-   @FXML
-   private Button searchButton;
+public class SearchGameScreenController implements ControlledScreen {
+	// FXML Components.
+	@FXML
+	private Button searchButton;
 
-   @FXML
-   private TextField minKarmaScoreTextField;
+	@FXML
+	private ComboBox<Integer> numberPlayersComboBox;
 
-   @FXML
-   private ComboBox gameTypeComboBox;
-   @FXML
-   private ComboBox numberPlayersComboBox;
+	@FXML
+	private CheckBox chatCheckBox;
 
-   @FXML
-   private CheckBox chatCheckBox;
+	// So we can set the screen's parent later on.
+	MainController parentController;
 
-   // So we can set the screen's parent later on.
-   MainController parentController;
+	/**
+	 * Initializes the controller class. Automatically called after the FXML
+	 * file has been loaded.
+	 */
+	@FXML
+	public void initialize() {
+		numberPlayersComboBox.getItems().add(2);
+		numberPlayersComboBox.getItems().add(3);
+		numberPlayersComboBox.getItems().add(4);
+		numberPlayersComboBox.getSelectionModel().select(0);
 
-   /**
-    * Initializes the controller class. Automatically called after the FXML file has been loaded.
-    */
-   @FXML
-   public void initialize()
-   {
-      // Set appropriate text if box is unchecked.
-      chatCheckBox.setOnAction(event ->
-      {
-         if (chatCheckBox.isSelected())
-            chatCheckBox.setText("On");
-         else
-            chatCheckBox.setText("Off");
-      });
+		// Set appropriate text if box is unchecked.
+		chatCheckBox.setOnAction(event -> {
+			if (chatCheckBox.isSelected())
+				chatCheckBox.setText("On");
+			else
+				chatCheckBox.setText("Off");
+		});
 
-      searchButton.setOnAction(event ->
-      {
-         // TO-DO: CHECK IF EVERYTHING WAS SELECTED PROPERLY. (VERIFY INPUT)
+		searchButton.setOnAction(event -> {
+			// TO-DO: CHECK IF EVERYTHING WAS SELECTED PROPERLY. (VERIFY INPUT)
 
-         // TO-DO: SET SEARCH CRITERA IN GAME LOBBY SINGLETON.
+				// TO-DO: SET SEARCH CRITERA IN GAME LOBBY SINGLETON.
 
-         // Display the lobby screen.
-         parentController.goToGameLobbyScreen();
-      });
-   }
+				// Display the lobby screen.
+				parentController.goToGameLobbyScreen();
+			});
+	}
 
-   /**
-    * This method will allow for the injection of each screen's parent.
-    */
-   @Override
-   public void setScreenParent(AbstractScreenController screenParent)
-   {
-      parentController = (MainController) screenParent;
-   }
+	/**
+	 * This method will allow for the injection of each screen's parent.
+	 */
+	@Override
+	public void setScreenParent(AbstractScreenController screenParent) {
+		parentController = (MainController) screenParent;
+	}
 }
