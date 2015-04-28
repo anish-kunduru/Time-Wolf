8da402cf32a05c436d764e80608c30afbd418dfe
Anish Kunduru <akunduru@iastate.edu>
2015-04-28 12:06:24 -0500

    Added chat boolean logic to CreateGameScreenController.

--- GameClient/src/createGame/CreateGameScreenController.java
+++ GameClient/src/createGame/CreateGameScreenController.java
@@ -16,66 +16,77 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class CreateGameScreenController implements ControlledScreen {
-	// FXML Components.
-	@FXML
-	private Button createGameButton;
+public class CreateGameScreenController implements ControlledScreen
+{
+   // FXML Components.
+   @FXML
+   private Button createGameButton;
 
-	@FXML
-	private TextField lobbyNameTextField;
-	@FXML
-	private TextField passwordTextField;
-	@FXML
-	private ComboBox<Integer> numberPlayersComboBox;
+   @FXML
+   private TextField lobbyNameTextField;
+   @FXML
+   private TextField passwordTextField;
+   @FXML
+   private ComboBox<Integer> numberPlayersComboBox;
 
-	@FXML
-	private CheckBox chatCheckBox;
+   @FXML
+   private CheckBox chatCheckBox;
 
-	// So we can set the screen's parent later on.
-	MainController parentController;
+   // So we can set the screen's parent later on.
+   MainController parentController;
 
-	/**
-	 * Initializes the controller class. Automatically called after the FXML
-	 * file has been loaded.
-	 */
-	@FXML
-	public void initialize() {
-		numberPlayersComboBox.getItems().add(2);
-		numberPlayersComboBox.getItems().add(3);
-		numberPlayersComboBox.getItems().add(4);
-		numberPlayersComboBox.getSelectionModel().select(0);
+   /**
+    * Initializes the controller class. Automatically called after the FXML file has been loaded.
+    */
+   @FXML
+   public void initialize()
+   {
+      numberPlayersComboBox.getItems().add(2);
+      numberPlayersComboBox.getItems().add(3);
+      numberPlayersComboBox.getItems().add(4);
+      numberPlayersComboBox.getSelectionModel().select(0);
 
-		// Set appropriate text if box is unchecked.
-		chatCheckBox.setOnAction(event -> {
-			if (chatCheckBox.isSelected())
-				chatCheckBox.setText("On");
-			else
-				chatCheckBox.setText("Off");
-		});
+      // Set appropriate text if box is unchecked.
+      chatCheckBox.setOnAction(event ->
+      {
+         if (chatCheckBox.isSelected())
+            chatCheckBox.setText("On");
+         else
+            chatCheckBox.setText("Off");
+      });
 
-		createGameButton.setOnAction(event -> {
-			int players = numberPlayersComboBox.getSelectionModel()
-					.getSelectedItem();
-			String name = lobbyNameTextField.getText();
+      createGameButton.setOnAction(event ->
+      {
+         int players = numberPlayersComboBox.getSelectionModel().getSelectedItem();
+         int id = 0;
+         boolean chatEnabled = chatCheckBox.isSelected();
+         String name = lobbyNameTextField.getText();
+         
+         try
+         {
+            id = MainModel.getModel().currentGameLobbyData().getGameManager().createGame(players, name);
+         }
+         catch (Exception e)
+         {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+         
+         // Set singleton information so that we can generate the game table view.
+         MainModel.getModel().currentGameLobbyData().setNumPlayers(players);
+         MainModel.getModel().currentGameLobbyData().setID(id);
+         MainModel.getModel().currentGameLobbyData().setChatEnabled(chatEnabled);
+         
+         parentController.goToGameTableScreen();
+      });
+   }
 
-			int id = 0;
-			try {
-				id = MainModel.getModel().currentGameLobbyData().getGameManager()
-						.createGame(players, name);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			MainModel.getModel().currentGameLobbyData().setID(id);
-			parentController.goToGameTableScreen();
-		});
-	}
-
-	/**
-	 * This method will allow for the injection of each screen's parent.
-	 */
-	@Override
-	public void setScreenParent(AbstractScreenController screenParent) {
-		parentController = (MainController) screenParent;
-	}
+   /**
+    * This method will allow for the injection of each screen's parent.
+    */
+   @Override
+   public void setScreenParent(AbstractScreenController screenParent)
+   {
+      parentController = (MainController) screenParent;
+   }
 }
